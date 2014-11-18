d95861859c57cca0d74b14f7bcffb23bf7096cb8
SimonTalaga <simon.talaga@gmail.com>
2014-11-18 17:43:00 +0100

    Standard layout

--- public/index.php
+++ public/index.php
@@ -1,1 +1,19 @@
-<?php phpinfo(); ?>
+<?php
+// Auto chargement des dépendences Composer
+require_once '../vendor/autoload.php';
+
+// Création d'un loader TWIG,
+// pour qu'il recherche les templates dans le répertoire application/views
+$loader = new Twig_Loader_Filesystem('../application/views');
+
+// Création d'une instance de Twig
+$twig = new Twig_Environment($loader);
+
+$data = array('page_title' => 'MycroBlog Home',
+              'title'      => 'MycroBlog');
+
+// Compilation et Affichage du template (index.twig)
+// Dans le fichier index.twig, le code {{ name }}
+// sera remplacé par sa valeur dans le tableau ("World")
+
+echo $twig->render('index.twig', $data);
