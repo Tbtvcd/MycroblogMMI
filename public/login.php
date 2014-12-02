76c5b6ff2709e4a81e44daed080b13481cbd7bc1
SimonTalaga <simon.talaga@gmail.com>
2014-12-02 17:15:10 +0100

    User can login

---
+++ public/login.php
@@ -0,0 +1,31 @@
+<?php
+session_start();
+// Auto chargement des dépendences Composer
+require_once '../vendor/autoload.php';
+
+// Création d'un loader TWIG,
+// pour qu'il recherche les templates dans le répertoire application/views
+$loader = new Twig_Loader_Filesystem('../application/views');
+
+// Création d'une instance de Twig
+$twig = new Twig_Environment($loader);
+
+if(isset($_SESSION['username'])) {
+    $data = array(
+        'username' => $_SESSION['username'],
+        'page_title' => 'MycroBlog Home',
+        'title'    => 'MycroBlog');
+}
+
+else {
+    $data = array(
+        'page_title' => 'MycroBlog Home',
+        'title'      => 'MycroBlog');
+}
+
+
+// Compilation et Affichage du template (index.twig)
+// Dans le fichier index.twig, le code {{ name }}
+// sera remplacé par sa valeur dans le tableau ("World")
+echo $twig->render('login.twig', $data);
+
--- public/register.php
+++ public/register.php
@@ -1,19 +1,33 @@
 <?php
-// Auto chargement des dépendences Composer
-require_once '../vendor/autoload.php';
+session_start();
 
-// Création d'un loader TWIG,
-// pour qu'il recherche les templates dans le répertoire application/views
-$loader = new Twig_Loader_Filesystem('../application/views');
+if(isset($_POST['inputLogin']) && isset($_POST['inputEmail']) && isset($_POST['inputPassword']) && isset($_POST['confirmPassword'])) {
+    require '../config/databaseCredentials.php';
+    $db =  new mysqli($dbc['host'], $dbc['user'], $dbc['password'], $dbc['dbName']);
+    $db->query("INSERT INTO users (email, login, password) VALUES ('". $_POST['inputEmail'] ."', '". $_POST['inputLogin'] ."', '". $_POST['inputPassword'] ."')");
 
-// Création d'une instance de Twig
-$twig = new Twig_Environment($loader);
+    $_SESSION['username'] = $_POST['inputLogin'];
+    header('Location: login.php');
+}
 
-$data = array('page_title' => 'MycroBlog Home',
-    'title'      => 'MycroBlog');
+else {
+    // Auto chargement des dépendences Composer
+    require_once '../vendor/autoload.php';
 
-// Compilation et Affichage du template (index.twig)
-// Dans le fichier index.twig, le code {{ name }}
-// sera remplacé par sa valeur dans le tableau ("World")
+    // Création d'un loader TWIG,
+    // pour qu'il recherche les templates dans le répertoire application/views
+    $loader = new Twig_Loader_Filesystem('../application/views');
 
-echo $twig->render('register.twig', $data);
+    // Création d'une instance de Twig
+    $twig = new Twig_Environment($loader);
+
+    $data = array(
+        'page_title' => 'MycroBlog Home',
+        'title'      => 'MycroBlog');
+
+    // Compilation et Affichage du template (index.twig)
+    // Dans le fichier index.twig, le code {{ name }}
+    // sera remplacé par sa valeur dans le tableau ("World")
+    echo $twig->render('register.twig', $data);
+}
+
